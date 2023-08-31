6f326391f81dfd811cc1baaf442025af686fefd3
InevitablePG <inavitablepg@gmail.com>
2023-08-31 06:36:50 +0000

    bulk images, services ui, booking form etc...

--- main/forms.py
+++ main/forms.py
@@ -1,8 +1,31 @@
 from django import forms
 from .models import Testimonial
 
+class BookingForm(forms.Form):
+    ISSUE_CHOICES = [
+        ('', ''),
+        ('Starter Repair', 'Starter Repair'),
+        ('Alternator Repairs', 'Alternator Repairs'),
+        ('Wiring & Rewinding', 'Wiring & Rewinding'),
+        ('Auto Electrical Spares', 'Auto Electrical Spares'),
+        ('Battery Testing & Charging', 'Battery Testing & Charging'),
+        ('Car Service (Minor/Major)', 'Car Service (Minor/Major)'),
+        ('Engine Overhaul', 'Engine Overhaul'),
+        ('Gearbox Repairs', 'Gearbox Repairs'),
+        ('Clutch Repairs', 'Clutch Repairs'),
+        ('Suspension Repairs', 'Suspension Repairs'),
+        ('Car Diagnosis', 'Car Diagnosis')
+    ]
+    
+    full_name = forms.CharField(max_length=100, required=True, widget=forms.TextInput(attrs={'placeholder': 'First and Last Name'}))
+    email = forms.EmailField(max_length=100, required=False, widget=forms.EmailInput(attrs={'placeholder': 'example@example.com'}))
+    phone_number = forms.CharField(max_length=20, required=True, widget=forms.TextInput(attrs={'placeholder': 'Phone Number'}))
+    issues = forms.ChoiceField(choices=ISSUE_CHOICES, required=True)
+    message = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Your message here', 'rows': 4}))
+
+
 
 class CommentForm(forms.ModelForm):
-	class Meta:
-		model = Testimonial
-		fields = ['city', 'text']
+    class Meta:
+        model = Testimonial
+        fields = ['city', 'text']
--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib import messages
 from .models import Testimonial, Gallery
-from .forms import CommentForm
+from .forms import CommentForm, BookingForm
 from django.views.generic import (
     ListView,
     DetailView,
@@ -13,40 +13,40 @@ from django.views.generic import (
 
 
 def home(request):
-	return render(request, 'main/home.html', {'title': 'EXCSERPRO: Excellent Service Provider'})
+    return render(request, 'main/home.html', {'title': 'EXCSERPRO: Excellent Service Provider'})
 
 
 def about(request):
-	return render(request, 'main/about.html', {'title': 'EXCSERPRO: About Us'})
+    return render(request, 'main/about.html', {'title': 'EXCSERPRO: About Us'})
 
 
 def services(request):
-	return render(request, 'main/services.html', {'title': 'EXCSERPRO: Our Services'})
+    return render(request, 'main/services.html', {'title': 'EXCSERPRO: Our Services'})
 
 
 class GalleryListView(ListView):
-	model = Gallery
-	template_name = 'main/gallery.html'  # <app>/<model>_<viewtype>.html
-	ordering = ['-date_posted']
+    model = Gallery
+    template_name = 'main/gallery.html'  # <app>/<model>_<viewtype>.html
+    ordering = ['-date_posted']
 
-	def get_context_data(self, **kwargs):
-	    context = super().get_context_data(**kwargs)
-	    context['title'] = 'EXCSERPRO: Gallery'
-	    return context
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'EXCSERPRO: Gallery'
+        return context
 
 
 class GalleryCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
-	model = Gallery
-	fields = ['image']
-	template_name = 'blog/new_post.html'
-	success_url = '/gallery'
+    model = Gallery
+    fields = ['image']
+    template_name = 'blog/new_post.html'
+    success_url = '/gallery'
 
-	def form_valid(self, form):
-		form.instance.author = self.request.user
-		return super().form_valid(form)
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
-	def test_func(self):
-	    return self.request.user.is_staff
+    def test_func(self):
+        return self.request.user.is_staff
 
 
 class GalleryDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
@@ -59,26 +59,44 @@ class GalleryDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
 
 
 def testimonals(request):
-	testimonals = Testimonial.objects.all().order_by('-date_posted')
+    testimonals = Testimonial.objects.all().order_by('-date_posted')
 
-	if request.method == 'POST':
-		form = CommentForm(request.POST)
-		if not request.user.is_authenticated:
-		    next_page = request.path
-		    messages.info(request, f'Please login first, or register if you dont have an account.')
-		    return redirect(f'/login/?next={next_page}')
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if not request.user.is_authenticated:
+            next_page = request.path
+            messages.info(request, f'Please login first, or register if you dont have an account.')
+            return redirect(f'/login/?next={next_page}')
 
-		if form.is_valid():
-		    testimonial = form.save(commit=False)
-		    testimonial.author = request.user  # Assuming you are using the built-in authentication system
-		    testimonial.save()
-		    messages.success(request, f'Your comment has been posted!')
-		    return redirect('Testimonals')
-	else:
-	    form = CommentForm()
+        if form.is_valid():
+            testimonial = form.save(commit=False)
+            testimonial.author = request.user  # Assuming you are using the built-in authentication system
+            testimonial.save()
+            messages.success(request, f'Your comment has been posted!')
+            return redirect('Testimonals')
+    else:
+        form = CommentForm()
 
-	return render(request, 'main/testimonals.html', {'form': form, 'testimonals': testimonals})
+    return render(request, 'main/testimonals.html', {'form': form, 'testimonals': testimonals})
 
 
 def contact(request):
-	return render(request, 'main/contact.html', {'title': 'EXCSERPRO: Contact Us'})
+    if request.method == "POST":
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            # Access the submitted data
+            
+            full_name = form.cleaned_data['full_name']
+            email = form.cleaned_data['email']
+            phone_number = form.cleaned_data['phone_number']
+            issues = form.cleaned_data['issues']
+            message = form.cleaned_data['message']
+            print(full_name, email, phone_number, issues, message)
+    else:
+        form = BookingForm()
+        
+    context = {
+        'form': form,
+        'title': 'EXCSERPRO: Contact Us'
+    }
+    return render(request, 'main/contact.html', context)
